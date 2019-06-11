ab23933f108e7e8b6d9f9007675e901a3c117447
minalkewat <minalkewat@gmail.com>
2019-06-11 17:18:38 +0530

    initial commit

--- Animateconstraint/ViewController.swift
+++ Animateconstraint/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var VCcontainerView: UIView!
+    @IBOutlet weak var viewBottomConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.VCcontainerView.layer.cornerRadius = 10
+        viewBottomConstraint.constant = -UIScreen.main.bounds.size.height
+        self.perform(#selector(bringUpView), with: nil, afterDelay: 0.4)
+    }
+    
+    @objc func bringUpView(){
+        UIView.animate(withDuration: 1.0) {
+            self.viewBottomConstraint.constant = 10
+            self.view.layoutIfNeeded()
+        }
     }
 
 
